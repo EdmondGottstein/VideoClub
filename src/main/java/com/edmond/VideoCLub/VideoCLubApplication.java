7d8889023879b8e823d29ca89c5da8c888758034
EdmondG <gottstein.edi@gmail.com>
2020-01-24 13:49:29 +0100

    Spring project initialized

---
+++ src/main/java/com/edmond/VideoCLub/VideoCLubApplication.java
@@ -0,0 +1,13 @@
+package com.edmond.VideoCLub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCLubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCLubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edmond/VideoCLub/VideoCLubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edmond.VideoCLub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCLubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
